ab7cd208a8bfb8334b4156bd08082e69cab60b51
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2024-03-01 18:49:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+const ageInput = document.getElementById("age");
+const nameInput = document.getElementById("name");
+const submitBtn = document.getElementById("btn");
+
+submitBtn.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  const age = parseInt(ageInput.value);
+  const name = nameInput.value.trim();
+
+  await delay(4000);
+
+  if (age >= 18) {
+    alert(`Welcome, ${name}. You can vote.`);
+  } else {
+    alert(`Oh sorry ${name}. You aren't old enough.`);
+  }
+});
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
